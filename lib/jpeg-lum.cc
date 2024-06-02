95f3199bf72eb90a4696190f7a9c8e510fb6ea08
naixx <r.chekan@gmail.com>
2024-06-02 05:06:32 +0300

    Add CMakeLists.txt and reformat code

--- lib/jpeg-lum.cc
+++ lib/jpeg-lum.cc
@@ -14,142 +14,141 @@ double luminance;
 double clipped;
 
 #define LUT_LENGTH 33
-struct lut_t {
-  double x;
-  double y;
-};
-lut_t lut[LUT_LENGTH] = 
+
+struct lut_t
 {
-  {4.793865/4,  -7},
-  {4.793865/2,  -6},
-  {4.793865,  -5},
-  {6.056875,  -4.66666666666667},
-  {7.697362,  -4.33333333333333},
-  {9.780035,  -4},
-  {12.092661, -3.66666666666667},
-  {15.089602, -3.33333333333333},
-  {18.683953, -3},
-  {23.421734, -2.66666666666667},
-  {28.643862, -2.33333333333333},
-  {35.019192, -2},
-  {42.607219, -1.66666666666667},
-  {52.126612, -1.33333333333333},
-  {63.956853, -1},
-  {76.885797, -0.666666666666667},
-  {89.6882,   -0.333333333333333},
-  {103.382354,  0},
-  {118.336503,  0.333333333333333},
-  {135.244366,  0.666666666666667},
-  {147.969053,  1},
-  {162.615242,  1.33333333333333},
-  {182.654258,  1.66666666666667},
-  {194.224312,  2},
-  {206.764022,  2.33333333333333},
-  {213.891809,  2.66666666666667},
-  {230.007117,  3},
-  {238.483315,  3.33333333333333},
-  {246.258409,  3.66666666666667},
-  {249.447248,  4},
-  {249.495413,  4.33333333333333},
-  {252,  5},
-  {253,  6}
+    double x;
+    double y;
 };
 
-double lum(double x)
+lut_t lut[LUT_LENGTH] =
 {
+    {4.793865 / 4, -7},
+    {4.793865 / 2, -6},
+    {4.793865, -5},
+    {6.056875, -4.66666666666667},
+    {7.697362, -4.33333333333333},
+    {9.780035, -4},
+    {12.092661, -3.66666666666667},
+    {15.089602, -3.33333333333333},
+    {18.683953, -3},
+    {23.421734, -2.66666666666667},
+    {28.643862, -2.33333333333333},
+    {35.019192, -2},
+    {42.607219, -1.66666666666667},
+    {52.126612, -1.33333333333333},
+    {63.956853, -1},
+    {76.885797, -0.666666666666667},
+    {89.6882, -0.333333333333333},
+    {103.382354, 0},
+    {118.336503, 0.333333333333333},
+    {135.244366, 0.666666666666667},
+    {147.969053, 1},
+    {162.615242, 1.33333333333333},
+    {182.654258, 1.66666666666667},
+    {194.224312, 2},
+    {206.764022, 2.33333333333333},
+    {213.891809, 2.66666666666667},
+    {230.007117, 3},
+    {238.483315, 3.33333333333333},
+    {246.258409, 3.66666666666667},
+    {249.447248, 4},
+    {249.495413, 4.33333333333333},
+    {252, 5},
+    {253, 6}
+};
+
+double lum(double x) {
     int i;
 
-    if(x < lut[0].x) return lut[0].y - 1;
-    if(x > lut[LUT_LENGTH-1].x) return lut[LUT_LENGTH-1].y + 1;
+    if (x < lut[0].x) return lut[0].y - 1;
+    if (x > lut[LUT_LENGTH - 1].x) return lut[LUT_LENGTH - 1].y + 1;
 
-    for( i = 0; i < LUT_LENGTH-1; i++ )
-    {
-        if ( lut[i].x <= x && lut[i+1].x >= x )
-        {
+    for (i = 0; i < LUT_LENGTH - 1; i++) {
+        if (lut[i].x <= x && lut[i + 1].x >= x) {
             double diffx = x - lut[i].x;
-            double diffn = lut[i+1].x - lut[i].x;
+            double diffn = lut[i + 1].x - lut[i].x;
 
-            return lut[i].y + ( lut[i+1].y - lut[i].y ) * diffx / diffn; 
+            return lut[i].y + (lut[i + 1].y - lut[i].y) * diffx / diffn;
         }
     }
     printf("error - not found\n");
     return 0; // Not in Range
 }
 
-int read_jpeg_file(char *filename)
-{
-  struct jpeg_decompress_struct cinfo;
-  struct jpeg_error_mgr jerr;
-  JSAMPROW row_pointer[1];
-  FILE *infile = fopen(filename, "rb");
-  unsigned int i = 0, component = 0;
-  if (!infile) {
-      printf("Error opening jpeg file %s\n!", filename);
-      return -1;
-  }
-  cinfo.err = jpeg_std_error(&jerr);
-  jpeg_create_decompress(&cinfo);
-  jpeg_stdio_src(&cinfo, infile);
-  jpeg_read_header(&cinfo, TRUE);
-  jpeg_start_decompress(&cinfo);
+int read_jpeg_file(char* filename) {
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    JSAMPROW row_pointer[1];
+    FILE* infile = fopen(filename, "rb");
+    unsigned int i = 0, component = 0;
+    if (!infile) {
+        printf("Error opening jpeg file %s\n!", filename);
+        return -1;
+    }
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_decompress(&cinfo);
+    jpeg_stdio_src(&cinfo, infile);
+    jpeg_read_header(&cinfo, TRUE);
+    jpeg_start_decompress(&cinfo);
 
-  width = cinfo.output_width;
-  height = cinfo.output_height;
-  luminance = 0.0;
-  size = cinfo.output_width*cinfo.output_height*cinfo.num_components*sizeof(unsigned int);
-  memset(histogram, 0, sizeof(int)*256);
-  row_pointer[0] = (unsigned char *)malloc(cinfo.output_width*cinfo.num_components);
-  unsigned long total_count = 0;
-  
-  unsigned int row_number = 0;
-  int skip_rows = 0;
-  float ratio = (float)width / (float)height;
-  if(ratio < 1.75) {
-    skip_rows = (height-((float)width)/1.75)/2;
-    //printf("skipping %d rows on top and bottom", skip_rows);
-  }
-  
-  while (cinfo.output_scanline < cinfo.image_height) {
-      row_number++;
-      jpeg_read_scanlines( &cinfo, row_pointer, 1 );
-      if(row_number <= skip_rows) continue;
-      if(row_number > height - skip_rows) continue;
-      for (i=0; i<cinfo.image_width;i+=cinfo.num_components) {
-          unsigned int m = 0;
-          for(component=0;component<cinfo.num_components;component++) {
-              if(component < 3) {
-                  unsigned int p = row_pointer[0][i + component];
-                  if(p > m) m = p;
-              }
-          }
-          if(m > 255) m = 255;
-          histogram[m]++;
-          total_count++;
-      }
-  }
+    width = cinfo.output_width;
+    height = cinfo.output_height;
+    luminance = 0.0;
+    size = cinfo.output_width * cinfo.output_height * cinfo.num_components * sizeof(unsigned int);
+    memset(histogram, 0, sizeof(int) * 256);
+    row_pointer[0] = (unsigned char*)malloc(cinfo.output_width * cinfo.num_components);
+    unsigned long total_count = 0;
 
-  unsigned long count = 0, sum = 0;
-  luminance = 0.0;
-  hist_max = 0;
-  for(int i = 0; i < 256; i++) {
-    sum += histogram[i];
-    if(histogram[i] > hist_max) hist_max = histogram[i];
-    if(sum < total_count / 2) continue; // only look at the upper 50%
-    luminance += lum((float)i) * (float) histogram[i];
-    count += histogram[i];
-  }
+    unsigned int row_number = 0;
+    int skip_rows = 0;
+    float ratio = (float)width / (float)height;
+    if (ratio < 1.75) {
+        skip_rows = (height - ((float)width) / 1.75) / 2;
+        //printf("skipping %d rows on top and bottom", skip_rows);
+    }
 
-  clipped = (double)histogram[255] / (double)count;
-  luminance /= (double)count;
-  jpeg_finish_decompress(&cinfo);
-  jpeg_destroy_decompress(&cinfo);
-  free(row_pointer[0]);
-  fclose(infile);
-  return 1;
+    while (cinfo.output_scanline < cinfo.image_height) {
+        row_number++;
+        jpeg_read_scanlines(&cinfo, row_pointer, 1);
+        if (row_number <= skip_rows) continue;
+        if (row_number > height - skip_rows) continue;
+        for (i = 0; i < cinfo.image_width; i += cinfo.num_components) {
+            unsigned int m = 0;
+            for (component = 0; component < cinfo.num_components; component++) {
+                if (component < 3) {
+                    unsigned int p = row_pointer[0][i + component];
+                    if (p > m) m = p;
+                }
+            }
+            if (m > 255) m = 255;
+            histogram[m]++;
+            total_count++;
+        }
+    }
+
+    unsigned long count = 0, sum = 0;
+    luminance = 0.0;
+    hist_max = 0;
+    for (int i = 0; i < 256; i++) {
+        sum += histogram[i];
+        if (histogram[i] > hist_max) hist_max = histogram[i];
+        if (sum < total_count / 2) continue; // only look at the upper 50%
+        luminance += lum((float)i) * (float)histogram[i];
+        count += histogram[i];
+    }
+
+    clipped = (double)histogram[255] / (double)count;
+    luminance /= (double)count;
+    jpeg_finish_decompress(&cinfo);
+    jpeg_destroy_decompress(&cinfo);
+    free(row_pointer[0]);
+    fclose(infile);
+    return 1;
 }
 
 #ifndef BUILDING_NODE_EXTENSION
-  #define BUILDING_NODE_EXTENSION
+#define BUILDING_NODE_EXTENSION
 #endif
 
 #include <node.h>
@@ -157,64 +156,64 @@ int read_jpeg_file(char *filename)
 using namespace v8;
 
 Handle<Value> CreateObject(const Arguments& args) {
-  HandleScope scope;
+    HandleScope scope;
 
-  Local<Object> obj = Object::New();
-  Local<Array> histArray = Array::New(256);
+    Local<Object> obj = Object::New();
+    Local<Array> histArray = Array::New(256);
 
-  for (unsigned int i = 0; i < 256; i++) histArray->Set(i, Number::New((histogram[i]*16)/((hist_max*16)/256))); // compress to 256 max, integer math for speed
+    for (unsigned int i = 0; i < 256; i++) histArray->Set(i, Number::New((histogram[i] * 16) / ((hist_max * 16) / 256)));
+    // compress to 256 max, integer math for speed
 
-  obj->Set(String::NewSymbol("histogram"), histArray);
-  obj->Set(String::NewSymbol("luminance"), Number::New(luminance));
-  obj->Set(String::NewSymbol("clipped"), Number::New(clipped));
-  obj->Set(String::NewSymbol("width"), Number::New(width));
-  obj->Set(String::NewSymbol("height"), Number::New(height));
+    obj->Set(String::NewSymbol("histogram"), histArray);
+    obj->Set(String::NewSymbol("luminance"), Number::New(luminance));
+    obj->Set(String::NewSymbol("clipped"), Number::New(clipped));
+    obj->Set(String::NewSymbol("width"), Number::New(width));
+    obj->Set(String::NewSymbol("height"), Number::New(height));
 
-  return obj;
+    return obj;
 }
 
 Handle<Value> Read(const Arguments& args) {
-  HandleScope scope;
+    HandleScope scope;
 
-  Local<Function> callback = Local<Function>::Cast(args[1]);
+    Local<Function> callback = Local<Function>::Cast(args[1]);
 
-  if (args.Length() < 2) {
-    Local<Value> err = Exception::Error(String::New("Specify an image filename to read"));    
-    Local<Value> argv[] = { err };
+    if (args.Length() < 2) {
+        Local<Value> err = Exception::Error(String::New("Specify an image filename to read"));
+        Local<Value> argv[] = {err};
 
-    callback->Call(Context::GetCurrent()->Global(), 1, argv);
+        callback->Call(Context::GetCurrent()->Global(), 1, argv);
 
-    return scope.Close(Undefined());
-  }
+        return scope.Close(Undefined());
+    }
 
-  String::AsciiValue string(args[0]);
-  char *filename = (char *) malloc(string.length() + 1);
-  strcpy(filename, *string);
+    String::AsciiValue string(args[0]);
+    char* filename = (char*)malloc(string.length() + 1);
+    strcpy(filename, *string);
 
-  if (read_jpeg_file(filename)) {
-    Handle<Value> value = CreateObject(args);
-    Local<Value> argv[] = {
+    if (read_jpeg_file(filename)) {
+        Handle<Value> value = CreateObject(args);
+        Local<Value> argv[] = {
             Local<Value>::New(Null()),
             Local<Value>::New(value),
-    };
-    callback->Call(Context::GetCurrent()->Global(), 2, argv);
-    return scope.Close(value);
-  }
-  else {
-    Local<Value> err = Exception::Error(String::New("Error reading image file"));    
-    Local<Value> argv[] = { err };
+        };
+        callback->Call(Context::GetCurrent()->Global(), 2, argv);
+        return scope.Close(value);
+    } else {
+        Local<Value> err = Exception::Error(String::New("Error reading image file"));
+        Local<Value> argv[] = {err};
 
-    callback->Call(Context::GetCurrent()->Global(), 1, argv);
-    
-    return scope.Close(Undefined());
-  }
+        callback->Call(Context::GetCurrent()->Global(), 1, argv);
 
-  return scope.Close(CreateObject(args));
+        return scope.Close(Undefined());
+    }
+
+    return scope.Close(CreateObject(args));
 }
 
 void init(Handle<Object> exports) {
-  exports->Set(String::NewSymbol("read"),
-      FunctionTemplate::New(Read)->GetFunction());
+    exports->Set(String::NewSymbol("read"),
+                 FunctionTemplate::New(Read)->GetFunction());
 }
 
 NODE_MODULE(jpeglum, init)
